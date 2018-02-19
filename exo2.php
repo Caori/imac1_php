a0d0cf979ac2faf367c4118bf15d3ae17a85e3aa
Caori <naiko.genevaux@gmail.com>
2018-02-19 15:46:18 +0100

    first commit

---
+++ exo2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <meta charset="utf-8" />
+     <title> Exo 2 </title>
+</head>
+<body>
+
+<?php
+     $prenom = "Solane";
+     $nom = "Genevaux";
+     $ville = "Tournefeuille";
+     $age = 20;
+
+if ($age == 1) {
+     echo "<div>Bonsoir, je m'appelle $prenom $nom, je viens de $ville et j'ai $age an.</div>";
+}
+else {
+     echo "<div>Bonsoir, je m'appelle $prenom $nom, je viens de $ville et j'ai $age ans.</div>";
+}
+?>
+
+
+</body>
+</html>
