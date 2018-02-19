76be35043885126ab1c6ad51a2ba80ee5454174b
Caori <naiko.genevaux@gmail.com>
2018-02-19 16:45:15 +0100

    td1

--- TD1/exo2.php
+++ TD1/exo2.php
@@ -18,6 +18,31 @@ if ($age == 1) {
 else {
      echo "<div>Bonsoir, je m'appelle $prenom $nom, je viens de $ville et j'ai $age ans.</div>";
 }
+
+$personne1 = array(
+  "prenom" => "John",
+  "nom" => "Egbert",
+  "ville" => "LOWAS",
+  "age" => 14
+);
+
+$personne2["prenom"] = "Jade";
+$personne2["nom"] = "Harley";
+$personne2["ville"] = "LOFAF";
+$personne2["age"] = 15;
+
+//var_dump($personne2);
+
+if ($personne["age"] == 1) {
+     echo "<div>Bonsoir, je m'appelle {$personne2["prenom"]} {$personne2["nom"]}, je viens de
+     {$personne2["ville"]} et j'ai {$personne2["age"]} an.</div>";
+}
+else {
+     echo "<div>Bonsoir, je m'appelle {$personne2["prenom"]} {$personne2["nom"]}, je viens de
+     {$personne2["ville"]} et j'ai {$personne2["age"]} ans.</div>";
+}
+
+
 ?>
 
 
